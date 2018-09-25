25fce2f604176fa38464a8d3d290e5d280c81485
Florian Courgey (Fresh) <florian@floriancourgey.com>
2018-09-25 17:58:00 -0400

    + xml2csv sample

---
+++ xml2csv/sample1.xml.js
@@ -0,0 +1,38 @@
+var sample1xml =
+'<?xml version="1.0" encoding="UTF-8"?>\n'+
+'\n'+
+'<bookstore>\n'+
+'\n'+
+'<book category="cooking">\n'+
+'  <title lang="en">Everyday Italian</title>\n'+
+'  <author>Giada De Laurentiis</author>\n'+
+'  <year>2005</year>\n'+
+'  <price>30.00</price>\n'+
+'</book>\n'+
+'\n'+
+'<book category="children">\n'+
+'  <title lang="en">Harry Potter</title>\n'+
+'  <author>J K. Rowling</author>\n'+
+'  <year>2005</year>\n'+
+'  <price>29.99</price>\n'+
+'</book>\n'+
+'\n'+
+'<book category="web">\n'+
+'  <title lang="en">XQuery Kick Start</title>\n'+
+'  <author>James McGovern</author>\n'+
+'  <author>Per Bothner</author>\n'+
+'  <author>Kurt Cagle</author>\n'+
+'  <author>James Linn</author>\n'+
+'  <author>Vaidyanathan Nagarajan</author>\n'+
+'  <year>2003</year>\n'+
+'  <price>49.99</price>\n'+
+'</book>\n'+
+'\n'+
+'<book category="web">\n'+
+'  <title lang="en">Learning XML</title>\n'+
+'  <author>Erik T. Ray</author>\n'+
+'  <year>2003</year>\n'+
+'  <price>39.95</price>\n'+
+'</book>\n'+
+'\n'+
+'</bookstore>';
