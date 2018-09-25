2bb9bf981fecba8395e601c23899cc56de439df3
Florian Courgey (Fresh) <florian@floriancourgey.com>
2018-09-25 17:57:52 -0400

    + xml2csv

---
+++ xml2csv/xml2csv.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+  el: '#xml2csv',
+  data:{
+    message: sample1xml,
+    xml: null,
+    loopXpath: '/bookstore/book',
+    cols: [
+      {title:'Title', xpath:'title'},
+      {title:'Author', xpath:'author'},
+    ],
+    results:[],
+  },
+  methods:{
+    generate: function(){
+      // parse
+      this.xml = (new DOMParser()).parseFromString(app.message, 'text/xml');
+      // get root loop xpath
+      var nodes = document.evaluate(this.loopXpath, this.xml, null, XPathResult.ANY_TYPE, null);
+      var node;
+      // reset results
+      this.results = [];
+      // for each node, push to results
+      while(node = nodes.iterateNext()){
+        console.log(node);
+        this.results.push({
+          0: document.evaluate(this.cols[0].xpath, node, null, XPathResult.STRING_TYPE, null).stringValue,
+          1: document.evaluate(this.cols[1].xpath, node, null, XPathResult.STRING_TYPE, null).stringValue,
+        })
+      }
+    }
+  }
+});
+app.generate();
