0de5c78e6033c9686a81a8485858c5d5086442d2
Florian Courgey <florian@floriancourgey.com>
2018-09-29 15:05:01 -0400

    - Github ribbon

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ var appHeader = {
         </div><!--/.container-fluid -->\
       </nav>\
       <hr/>\
-      <a href="https://github.com/floriancourgey/tools" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>\
     </header>',
   methods: {url: function(x){return url(x)}}
 }
