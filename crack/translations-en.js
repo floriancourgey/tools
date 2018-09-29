fd8ee339d09d0cb7999d8eb5bd9558f3138b8000
Florian Courgey <florian@floriancourgey.com>
2018-09-29 12:38:03 -0400

    + crack translations (1%)

---
+++ crack/translations-en.js
@@ -0,0 +1,5 @@
+var translations = translations || {};
+translations['en'] = {
+  'title':'Crack & Hack',
+  'r2.title':'radare2 r2 disassembler for crackmes',
+};
---
+++ crack/translations-fr.js
@@ -0,0 +1,5 @@
+var translations = translations || {};
+translations['fr'] = {
+  'title':'Crack & Hack',
+  'r2.title':'désassembleur radare2 r2 pour les crackmes',
+};
